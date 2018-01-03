6e7e503d574dfe24cb2adac059be3f86efb3c0e7
digranate <dbarshev1@googlemail.com>
2018-01-03 19:14:21 +0300

    HW07

--- src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
@@ -1,48 +1,49 @@
 package ru.javawebinar.topjava.web.meal;
 
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.to.MealWithExceed;
 
+import java.net.URI;
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
 import java.util.List;
 
 @RestController
-@RequestMapping(MealRestController.REST_URL)
+@RequestMapping(path = MealRestController.REST_URL, produces = MediaType.APPLICATION_JSON_VALUE)
 public class MealRestController extends AbstractMealController {
-    public static final String REST_URL="/rest/meals";
+    public static final String REST_URL="/rest/profile/meals";
 
-    @GetMapping(value = "/delete/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(value = "/{id}")
+    public Meal get(@PathVariable("id") int id) {
+        return super.get(id);
+    }
+
+    @GetMapping()
+    public List<MealWithExceed> getAll() {
+        return super.getAll();
+    }
+    @DeleteMapping(value = "/{id}")
     public void delete(@PathVariable("id") int id) {
         super.delete(id);
     }
 
-    @GetMapping(value = "/update/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public Meal update(@PathVariable("id") int id) {
-        return super.get(id);
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Meal> createWithLocation(@RequestBody Meal meal) {
+              Meal created =      super.create(meal);
+        URI uriOfNewResource = ServletUriComponentsBuilder.fromCurrentContextPath()
+                .path(REST_URL + "/{id}")
+                .buildAndExpand(created.getId()).toUri();
+        return ResponseEntity.created(uriOfNewResource).body(created);
     }
 
-    @GetMapping(value = "/create", produces = MediaType.APPLICATION_JSON_VALUE)
-    public Meal create() {
-        Meal newMeal = new Meal(LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS), "", 1000);
-        return updateOrCreate(newMeal);
-    }
-
-    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public Meal updateOrCreate(@RequestBody Meal meal) {
-        /*Meal meal = new Meal(LocalDateTime.parse(request.
-                getParameter("dateTime")),
-                request.getParameter("description"),
-                Integer.valueOf(request.getParameter("calories")));
-*/
-        if (meal.isNew()) {
-            super.create(meal);
-        } else {
-            super.update(meal, meal.getId());
-        }
-        return meal;
+    @PutMapping(value = "/{id}", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public void update(@RequestBody Meal meal, @PathVariable int id) {
+            super.update(meal, id);
     }
 
     @PostMapping("/filter/{startDate}/{startTime}/{endDate}/{endTime}")
--- src/test/java/ru/javawebinar/topjava/web/meal/MealRestControllerTest.java
+++ src/test/java/ru/javawebinar/topjava/web/meal/MealRestControllerTest.java
@@ -1,11 +1,14 @@
 package ru.javawebinar.topjava.web.meal;
 
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.ResultActions;
+import ru.javawebinar.topjava.AuthorizedUser;
 import ru.javawebinar.topjava.MealTestData;
 import ru.javawebinar.topjava.TestUtil;
 import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.service.MealService;
 import ru.javawebinar.topjava.web.AbstractControllerTest;
 import ru.javawebinar.topjava.web.json.JsonUtil;
 
@@ -14,38 +17,27 @@ import java.time.Month;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static ru.javawebinar.topjava.MealTestData.MEAL1_ID;
+import static ru.javawebinar.topjava.MealTestData.MEAL2;
 import static ru.javawebinar.topjava.MealTestData.assertMatch;
 
 public class MealRestControllerTest extends AbstractControllerTest {
 
     private static final String REST_URL = MealRestController.REST_URL + '/';
-
+    @Autowired
+    MealService service;
     @Test
     public void delete() throws Exception {
-        mockMvc.perform(get(REST_URL + "delete/" + MEAL1_ID))
+        mockMvc.perform(get(REST_URL + MEAL1_ID))
                 .andExpect(status().isOk())
                 .andDo(print());
+
     }
 
-    @Test
-    public void update() throws Exception {
-         mockMvc.perform(get(REST_URL + "update/" + MEAL1_ID))
-        .andExpect(status().isOk())
-        .andDo(print())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8));
-    }
-
-    @Test
-    public void create() throws Exception {
-        mockMvc.perform(get(REST_URL + "create"))
-                .andExpect(status().isOk())
-                .andDo(print())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8));
-    }
 
     @Test
     public void postCreate() throws Exception {
@@ -54,7 +46,7 @@ public class MealRestControllerTest extends AbstractControllerTest {
                 mockMvc.perform(post(REST_URL)
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(JsonUtil.writeValue(expected)))
-                .andExpect(status().isOk())
+                .andExpect(status().is(201))
                 .andDo(print());
         Meal returned = TestUtil.readFromJson(action, Meal.class);
         expected.setId(returned.getId());
@@ -63,16 +55,15 @@ public class MealRestControllerTest extends AbstractControllerTest {
        }
 
     @Test
-    public void postUpdate() throws Exception {
-        Meal expected = new Meal(MEAL1_ID, LocalDateTime.of(2018, Month.JANUARY,1, 0,0,0), "dinner", 1500);
-        ResultActions action =
-                mockMvc.perform(post(REST_URL)
+    public void putUpdate() throws Exception {
+        Meal expected = new Meal(MEAL2.getId(), LocalDateTime.of(2018, Month.JANUARY,1, 0,0,0), "dinner", 1500);
+        mockMvc.perform(put(REST_URL + MEAL2.getId())
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(JsonUtil.writeValue(expected)))
-                        .andExpect(status().isOk())
-                        .andDo(print());
-        Meal returned = TestUtil.readFromJson(action, Meal.class);
+                        .andDo(print())
+                        .andExpect(status().isOk());
 
+        Meal returned = service.get(MEAL2.getId(), AuthorizedUser.id());
         assertMatch(returned, expected);
     }
 
